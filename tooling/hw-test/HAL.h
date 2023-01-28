e21b478a06f8d7fa1f7272b914e0aeb47a5fe9f2
Nick Benik <nbenik@gmail.com>
2023-01-28 16:17:02 -0500

    Updates HAL to prototype setup

--- tooling/hw-test/HAL.h
+++ tooling/hw-test/HAL.h
@@ -1,13 +1,13 @@
 
 // Buttons and keys
 #define DEBOUNCE_INTERVAL 20    // set debounce interval to 20ms
-#define RED_BTN_PIN     34
-#define RED_KEY_PIN     39
+#define RED_BTN_PIN     39
+#define RED_KEY_PIN     36
 #define BLUE_BTN_PIN    35
-#define BLUE_KEY_PIN    36
+#define BLUE_KEY_PIN    34
 #define BLUE_BTN_NO     // Industrial Blue buttons are normally open
-#define RED_BTN_NO      // ...for breadboard prototype testing
-//#define RED_BTN_NC      // Industrial Red buttons are normally closed
+//#define RED_BTN_NO      // ...for breadboard prototype testing
+#define RED_BTN_NC      // Industrial Red buttons are normally closed
 #define BLUE_KEY_NO     // Key switches are normally open ...
 #define RED_KEY_NO      // ...
 
@@ -15,17 +15,27 @@
 // Pins used by the Blue timer
 #define PIN_TMR_BLUE_CLK	4
 #define PIN_TMR_BLUE_DATA	13
-
 // Pins used by the Red timer
 #define PIN_TMR_RED_CLK		33
-#define PIN_TMR_RED_DATA    32
+#define PIN_TMR_RED_DATA  32
+
+// Pin used by Neopixels
+#define PIN_NEOPIXELS     27
+
+// Pin used for battery voltage
+#define PIN_BATTERY       14
+#define BATTERY_DISABLES_WIFI
 
 // Serial used by the DF Player module
-#define SERIAL_MP3			Serial2
+#define SERIAL_MP3			  Serial2
 
 // Define I2C Addresses
 #define I2C_ADDRESS_MASTER	0xef
-#define I2C_ADDRESS_LCD		0x3f
+#define I2C_ADDRESS_LCD		  0x3f
 #define I2C_ADDRESS_WIRECUT 0x39
 #define I2C_ADDRESS_UART    0x3a
 #define I2C_ADDRESS_KEYPAD  0x3b
+
+// I2C RFID module (with reset pin)
+#define I2C_ADDRESS_RFID    0x28
+#define I2C_RFID_RST        25
